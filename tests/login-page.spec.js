abcbfb2ca8b840b1b05dbc36de719cc7ce692125
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-10-10 17:28:59 -0400

    login content test, standard_user test, locked_out_user test

--- tests/login-page.spec.js
+++ tests/login-page.spec.js
@@ -1,23 +1,43 @@
 const { test, expect } = require("@playwright/test");
 
-// test("Has title", async ({ page }) => {
-//   await page.goto("https://www.saucedemo.com/");
-//   await expect(page).toHaveTitle(/Swag Labs/);
-// });
+const password = "secret_sauce";
 
-// test("Has title", async ({ page }) => {
-//   await expect(page.locator(".app_logo")).toHaveText("Swag Labs");
-//   console.log("YO");
-// });
+test("Check for login page content", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await expect(page.locator(".login_logo")).toHaveText("Swag Labs");
+  await expect(page.getByPlaceholder("Username")).toBeVisible();
+  await expect(page.getByPlaceholder("Password")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Login" })).toBeVisible();
+  await expect(page.locator("#login_credentials")).toBeVisible();
+  await expect(page.locator(".login_password")).toBeVisible();
+});
 
-test("Has other thing", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
-  await expect(
-    page
-      .locator(".inventory_item_label")
-      .first()
-      .locator(".inventory_item_name ")
-  ).toHaveText("Sauce Labs Backpack");
+test("standard_user should log in successfully", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill(password);
+  await page.locator("#login-button").click();
+  await page.waitForURL("https://www.saucedemo.com/inventory.html");
+});
+
+test("error text should appear for locked_out_user", async ({ page }) => {
+  const errorMessage = "Epic sadface: Sorry, this user has been locked out.";
+
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder("Username").fill("locked_out_user");
+  await page.getByPlaceholder("Password").fill(password);
+  await page.locator("#login-button").click();
+  await expect(page.getByText(errorMessage)).toHaveText(errorMessage);
+});
+
+test("error text should appear for locked_out_user", async ({ page }) => {
+  const errorMessage = "Epic sadface: Sorry, this user has been locked out.";
+
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder("Username").fill("locked_out_user");
+  await page.getByPlaceholder("Password").fill(password);
+  await page.locator("#login-button").click();
+  await expect(page.getByText(errorMessage)).toHaveText(errorMessage);
 });
 
 // Git
