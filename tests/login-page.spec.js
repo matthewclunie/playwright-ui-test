0e80f2e7da3f26277048a3a6e4f19a1868109928
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-10-11 17:03:17 -0400

    added JSON data, refactored login, tested incorrect logins

--- tests/login-page.spec.js
+++ tests/login-page.spec.js
@@ -1,6 +1,23 @@
 const { test, expect } = require("@playwright/test");
+const loginJSON = require("../Utils/login-info.json");
+const lockedOutJSON = require("../Utils/bad-login-info.json");
+const loginData = JSON.parse(JSON.stringify(loginJSON));
+const badUserData = JSON.parse(JSON.stringify(lockedOutJSON));
 
-const password = "secret_sauce";
+const errorMessageCheck = async (page) => {
+  await expect(page.locator(".svg-inline--fa").first()).toBeVisible();
+  await expect(page.locator(".svg-inline--fa").nth(1)).toBeVisible();
+  const errorButton = page.locator('[data-test="error-button"]');
+  await expect(errorButton).toBeVisible();
+  await errorButton.click();
+  await expect(errorButton).not.toBeVisible();
+};
+
+const login = async (userName, password) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.getByPlaceholder("Username").fill(userName);
+  await page.getByPlaceholder("Password").fill(password);
+};
 
 test("Check for login page content", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/");
@@ -12,34 +29,50 @@ test("Check for login page content", async ({ page }) => {
   await expect(page.locator(".login_password")).toBeVisible();
 });
 
-test("standard_user should log in successfully", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/");
-  await page.getByPlaceholder("Username").fill("standard_user");
-  await page.getByPlaceholder("Password").fill(password);
-  await page.locator("#login-button").click();
-  await page.waitForURL("https://www.saucedemo.com/inventory.html");
+loginData.forEach((data) => {
+  test(`${data.userName} should log in successfully`, async ({ page }) => {
+    await login(data.userName, data.password);
+    await page.locator("#login-button").click();
+    await page.waitForURL("https://www.saucedemo.com/inventory.html");
+  });
+});
+
+test("incorrect username correct password", async ({ page }) => {
+  await login(
+    badUserData.incorrect_userName.userName,
+    badUserData.incorrect_userName.password
+  );
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByRole("heading", { level: 3 })).toHaveText(
+    badUserData.incorrect_userName.errorMessage
+  );
+  await errorMessageCheck(page);
+});
+
+test("correct username incorrect password", async ({ page }) => {
+  await login(
+    badUserData.incorrect_password.userName,
+    badUserData.incorrect_password.password
+  );
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.locator("/svg-inline--fa").first()).toBeVisible();
+  await expect(page.locator("/svg-inline--fa").nth(1)).toBeVisible();
+  await expect(page.getByRole("heading", { level: 3 })).toHaveText(
+    badUserData.incorrect_userName.errorMessage
+  );
+  await errorMessageCheck(page);
 });
 
 test("error text should appear for locked_out_user", async ({ page }) => {
-  const errorMessage = "Epic sadface: Sorry, this user has been locked out.";
-
-  await page.goto("https://www.saucedemo.com/");
-  await page.getByPlaceholder("Username").fill("locked_out_user");
-  await page.getByPlaceholder("Password").fill(password);
+  await login(
+    badUserData.locked_out_user.userName,
+    badUserData.locked_out_user.password
+  );
   await page.locator("#login-button").click();
-  await expect(page.getByText(errorMessage)).toHaveText(errorMessage);
+  await expect(page.locator("/svg-inline--fa").first()).toBeVisible();
+  await expect(page.locator("/svg-inline--fa").nth(1)).toBeVisible();
+  await expect(page.getByText(errorMessage)).toHaveText(
+    lockedOutUserData.errorMessage
+  );
+  await errorMessageCheck(page);
 });
-
-test("error text should appear for locked_out_user", async ({ page }) => {
-  const errorMessage = "Epic sadface: Sorry, this user has been locked out.";
-
-  await page.goto("https://www.saucedemo.com/");
-  await page.getByPlaceholder("Username").fill("locked_out_user");
-  await page.getByPlaceholder("Password").fill(password);
-  await page.locator("#login-button").click();
-  await expect(page.getByText(errorMessage)).toHaveText(errorMessage);
-});
-
-// Git
-// Expect everything on login page. Put in auth for now?
-// Make an array for titles, descriptions, etc. loop through and make sure everything has what it needs
