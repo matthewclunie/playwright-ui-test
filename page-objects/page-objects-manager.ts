7882242ccb2d21afd2b3d779c75a8ab22c621b20
matthewclunie <matthew.clunie@hunterbusinessschool.edu>
2024-10-21 20:32:21 -0400

    began inventory page refactor

---
+++ page-objects/page-objects-manager.ts
@@ -0,0 +1,19 @@
+import { Page } from "playwright";
+
+import { InventoryPage } from "../page-objects/inventory-page-utils";
+
+class PageObjectsManager {
+  page: Page;
+  inventoryPage: InventoryPage;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.inventoryPage = new InventoryPage(page);
+  }
+
+  getInventoryPage() {
+    return this.inventoryPage;
+  }
+}
+
+module.exports = { PageObjectsManager };
--- tests/inventory-page.spec.ts
+++ tests/inventory-page.spec.ts
@@ -1,291 +1,118 @@
-import exp from "constants";
-import { Locator, Page } from "playwright";
+import { Page } from "playwright";
 
 const { test, expect } = require("@playwright/test");
-
-interface FooterLink {
-  identifier: string;
-  url: string;
-}
-
-const optionsJSON = require("../Utils/dropdown-info.json");
-const optionsData = JSON.parse(JSON.stringify(optionsJSON));
-
-const productsJSON = require("../Utils/product-info.json");
-const productsData = JSON.parse(JSON.stringify(productsJSON));
-
-// const checkProductDetails = async (locator: Locator, i: number) => {
-//   await expect(locator).toHaveText(productsData[i].title);
-//   await expect(locator).toHaveText(productsData[i].description);
-//   await expect(locator).toHaveText(productsData[i].price);
-// };
+const { PageObjectsManager } = require("../page-objects/page-objects-manager");
 
 test("Check for inventory page content", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
-  await expect(page.locator(".header_label")).toBeVisible();
-  await expect(page.locator(".shopping_cart_link")).toBeVisible();
-  await expect(page.locator(".app_logo")).toHaveText("Swag Labs");
-  await expect(page.locator(".bm-burger-button")).toBeVisible();
-  await expect(page.locator(".title")).toHaveText("Products");
-  await expect(page.locator(".product_sort_container")).toBeVisible();
-
-  const options = page.locator("option");
-  const optionsCount = await options.count();
-  for (let i = 0; i < optionsCount; i++) {
-    const option = await options.nth(i);
-    await expect(option).toHaveText(optionsData[i].text);
-    await expect(option).toHaveAttribute("value", optionsData[i].value);
-  }
-  await expect(options).toHaveCount(4);
-
-  const products = page.locator(".inventory_item");
-  const productsCount = await products.count();
-
-  for (let i = 0; i < productsCount; i++) {
-    const product = await products.nth(i);
-    await expect(product.locator(".inventory_item_name")).toHaveText(
-      productsData[i].title
-    );
-    await expect(product.locator(".inventory_item_desc")).toHaveText(
-      productsData[i].description
-    );
-    await expect(product.locator(".inventory_item_price")).toHaveText(
-      productsData[i].price
-    );
-    await expect(product.locator("img.inventory_item_img")).toHaveAttribute(
-      "src",
-      productsData[i].imageSrc
-    );
-  }
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
+  await inventoryPage.goToInventoryPage();
+  await inventoryPage.checkBasicPageContent();
+  await inventoryPage.checkForDropdownOptions();
+  await inventoryPage.checkForProductContent();
 });
 
 test("Check add to cart button works", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
-  const addToCartButtons = page.locator(".btn_inventory");
-  const addToCartButtonsCount = await addToCartButtons.count();
-  const cartBadge = page.locator(".shopping_cart_badge");
-  let badgeCount = 0;
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
+  await inventoryPage.goToInventoryPage();
 
-  expect(cartBadge).not.toBeVisible();
-
-  for (let i = 0; i < addToCartButtonsCount; i++) {
-    const addToCartButton = await addToCartButtons.nth(i);
-    await expect(addToCartButton).toHaveText("Add to cart");
-    await addToCartButton.click();
-    badgeCount++;
-    await expect(cartBadge).toHaveText(badgeCount.toString());
-  }
-
+  await inventoryPage.checkAddingItemsToCart();
   await page.locator(".shopping_cart_link").click();
-
-  const cartProducts = page.locator(".cart_item");
-  const cartProductsCount = await cartProducts.count();
-
-  for (let i = 0; i < cartProductsCount; i++) {
-    const cartProduct = await cartProducts.nth(i);
-    expect(cartProduct.locator(".inventory_item_name")).toHaveText(
-      productsData[i].title
-    );
-    expect(cartProduct.locator(".inventory_item_desc")).toHaveText(
-      productsData[i].description
-    );
-    expect(cartProduct.locator(".inventory_item_price")).toHaveText(
-      productsData[i].price
-    );
-  }
-
+  await inventoryPage.checkItemsInCart();
   await page.locator("#continue-shopping").click();
-
-  for (let i = 0; i < addToCartButtonsCount; i++) {
-    const addToCartButton = await addToCartButtons.nth(i);
-    await expect(addToCartButton).toHaveText("Remove");
-    await addToCartButton.click();
-    badgeCount--;
-    if (badgeCount > 0) {
-      await expect(cartBadge).toHaveText(badgeCount.toString());
-    } else {
-      await expect(cartBadge).not.toBeVisible();
-    }
-    await expect(addToCartButton).toHaveText("Add to cart");
-  }
+  await inventoryPage.checkRemovingItemsFromCart();
 });
 
 test("Check product links work", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
   const productLinks = page.locator(".inventory_item_name");
   const productLinkCount = await productLinks.count();
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
+  await inventoryPage.goToInventoryPage();
 
   for (let i = 0; i < productLinkCount; i++) {
     const productLink = await productLinks.nth(i);
     await productLink.click();
-    await expect(page).toHaveURL(
-      `https://www.saucedemo.com/inventory-item.html?id=${productsData[i].id}`
-    );
-    await expect(page.locator(".inventory_details_name")).toHaveText(
-      productsData[i].title
-    );
-    await expect(page.locator(".inventory_details_desc")).toHaveText(
-      productsData[i].description
-    );
-    await expect(page.locator(".inventory_details_price")).toHaveText(
-      productsData[i].price
-    );
-    expect(page.locator(".inventory_details_img")).toHaveAttribute(
-      "src",
-      productsData[i].imageSrc
-    );
-    await expect(page.locator("#add-to-cart")).toHaveText("Add to cart");
-    await page.locator("#add-to-cart").click();
-    await expect(page.locator(".shopping_cart_badge")).toHaveText("1");
-    await expect(page.locator("#remove")).toHaveText("Remove");
+    await inventoryPage.checkProductPageContent();
+
+    await inventoryPage.addItemFromProductPage();
     await page.locator(".shopping_cart_link").click();
-
-    expect(page.locator(".inventory_item_name")).toHaveText(
-      productsData[i].title
-    );
-    expect(page.locator(".inventory_item_desc")).toHaveText(
-      productsData[i].description
-    );
-    expect(page.locator(".inventory_item_price")).toHaveText(
-      productsData[i].price
-    );
-
+    await inventoryPage.checkProductDetails(page, i);
     await page.goBack();
-    await page.locator("#remove").click();
-    await expect(page.locator("#add-to-cart")).toHaveText("Add to cart");
-    await expect(page.locator(".shopping_cart_badge")).not.toBeVisible();
+    await inventoryPage.removeItemFromProductPage();
     await page.locator("#back-to-products").click();
   }
 });
 
 test("Check nav bar works", async ({ context, page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
-  await page.locator("#react-burger-menu-btn").click();
-  await page.locator("#about_sidebar_link").click();
-  await expect(page).toHaveURL("https://saucelabs.com/");
-  await page.goBack();
-  await page.locator("#add-to-cart-sauce-labs-backpack").click();
-  await expect(page.locator(".shopping_cart_badge")).toHaveText("1");
-  await page.locator("#react-burger-menu-btn").click();
-  await page.locator("#reset_sidebar_link").click();
-  await expect(page.locator(".shopping_cart_badge")).not.toBeVisible();
-  await page.locator("#react-burger-cross-btn").click();
-  await expect(page.locator(".bm-item-list")).not.toBeVisible();
-  await page.locator(".shopping_cart_link").click();
-  await expect(page).toHaveURL("https://www.saucedemo.com/cart.html");
-  await page.locator("#react-burger-menu-btn").click();
-  await page.locator("#inventory_sidebar_link").click();
-  await expect(page.locator(".bm-item-list")).not.toBeVisible();
-  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
-  const loggedInCookies = await context.cookies("https://www.saucedemo.com/");
-  await expect(loggedInCookies.length).toBeGreaterThan(0);
-  await page.locator("#react-burger-menu-btn").click();
-  await page.locator("#logout_sidebar_link").click();
-  await expect(page).toHaveURL("https://www.saucedemo.com/");
-  const loggedOutCookies = await context.cookies("https://www.saucedemo.com/");
-  await expect(loggedOutCookies.length).toBe(0);
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
+  await inventoryPage.goToInventoryPage();
+  await inventoryPage.checkAbout();
+  await inventoryPage.checkResetState();
+  await inventoryPage.checkNavClose();
+  await inventoryPage.checkAllItems();
+  await inventoryPage.checkLogout(context);
 });
 
 test("Check footer", async ({ page }) => {
   const footerText =
     "© 2024 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy";
 
-  const footerLinks: FooterLink[] = [
-    { identifier: ".social_twitter", url: "https://x.com/saucelabs?mx=2" },
-    {
-      identifier: ".social_facebook",
-      url: "https://www.facebook.com/saucelabs",
-    },
-    {
-      identifier: ".social_linkedin",
-      url: "https://www.linkedin.com/company/sauce-labs/",
-    },
-  ];
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
 
-  const checkFooterLink = async (
-    page: Page,
-    identifier: string,
-    url: string
-  ) => {
-    const newTabPromise = page.waitForEvent("popup");
-    await page.locator(identifier).click();
-    const newTab = await newTabPromise;
-    await expect(newTab).toHaveURL(url);
-    await newTab.close();
-  };
-
-  await page.goto("https://www.saucedemo.com/inventory.html");
-
-  for (const footerLink of footerLinks) {
-    await checkFooterLink(page, footerLink.identifier, footerLink.url);
-  }
-
-  await expect(page.locator(".footer_copy")).toHaveText(footerText);
+  await inventoryPage.goToInventoryPage();
+  await inventoryPage.checkFooterLinks();
+  await inventoryPage.checkFooterText(footerText);
 });
 
-test("Check sorting tab works", async ({ page }) => {
-  await page.goto("https://www.saucedemo.com/inventory.html");
-  const activeSelector = page.locator(".active_option");
-  const dropdownOptions = page.locator(".product_sort_container");
-  const productLabels = page.locator(".inventory_item_name ");
-  const productPrices = page.locator(".inventory_item_price");
-
-  const getProductLabelTexts = async () => {
-    return await productLabels.allInnerTexts();
-  };
-
-  const getProductPriceTexts = async () => {
-    return await productPrices.allInnerTexts();
-  };
-
-  const sortedProductLabels = [...(await getProductLabelTexts())].sort();
-  const revSortedProductLabels = [...sortedProductLabels].reverse();
-
-  const sortedPrices = [...(await getProductPriceTexts())].sort((a, b) => {
-    const priceA = parseFloat(a.replace("$", ""));
-    const priceB = parseFloat(b.replace("$", ""));
-    return priceA - priceB;
-  });
-
-  const revSortedPrices = [...sortedPrices].reverse();
+test("Check sorting dropdown works", async ({ page }) => {
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
+  await inventoryPage.goToInventoryPage();
+  const productLabels = await inventoryPage.getProductLabels();
+  const sortedLabels = inventoryPage.getSortedProductLabels(productLabels);
+  const revSortedLabels = inventoryPage.getRevSortedProductLabels(sortedLabels);
+  const productPrices = await inventoryPage.getProductPrices();
+  const sortedPrices = inventoryPage.getSortedPrices(productPrices);
+  const revSortedPrices = inventoryPage.getRevSortedPrices();
 
   const dropdownSelects = [
     {
       value: "az",
       text: "Name (A to Z)",
-      getTexts: getProductLabelTexts,
-      sortedTexts: sortedProductLabels,
+      getTexts: inventoryPage.getProductLabels,
+      sortedTexts: sortedLabels,
     },
     {
       value: "za",
       text: "Name (Z to A)",
-      getTexts: getProductLabelTexts,
-      sortedTexts: revSortedProductLabels,
+      getTexts: inventoryPage.getProductLabels,
+      sortedTexts: revSortedLabels,
     },
     {
       value: "lohi",
       text: "Price (low to high)",
-      getTexts: getProductPriceTexts,
+      getTexts: inventoryPage.getProductPrices,
       sortedTexts: sortedPrices,
     },
     {
       value: "hilo",
       text: "Price (high to low)",
-      getTexts: getProductPriceTexts,
+      getTexts: inventoryPage.getProductPrices,
       sortedTexts: revSortedPrices,
     },
   ];
 
-  for (const select of dropdownSelects) {
-    await dropdownOptions.selectOption(select.value);
-    await expect(activeSelector).toHaveText(select.text);
-    const texts = await select.getTexts();
-    await expect(texts).toEqual(select.sortedTexts);
-  }
+  await inventoryPage.checkDropdownFunctionality(dropdownSelects);
 });
 
 test("Check shopping cart works", async ({ page }) => {
   await page.goto("https://www.saucedemo.com/inventory.html");
+  const poManager = new PageObjectsManager(page);
+  const inventoryPage = poManager.getInventoryPage();
   const addToCartButtons = page.locator(".btn_inventory");
   const addToCartButtonsCount = await addToCartButtons.count();
   for (let i = 0; i < addToCartButtonsCount; i++) {
@@ -315,15 +142,7 @@ test("Check shopping cart works", async ({ page }) => {
 
   for (let i = 0; i < cartItemsCount; i++) {
     const cartItem = await cartItems.nth(i);
-    expect(cartItem.locator(".inventory_item_name")).toHaveText(
-      productsData[i].title
-    );
-    expect(cartItem.locator(".inventory_item_desc")).toHaveText(
-      productsData[i].description
-    );
-    expect(cartItem.locator(".inventory_item_price")).toHaveText(
-      productsData[i].price
-    );
+    await inventoryPage.checkProductDetails(cartItem, i);
     expect(cartItem.locator(".cart_button")).toHaveText("Remove");
   }
 });
